344384a6bb88c5ba0a5cc71729bb3f4ad617cfa3
Paridhi <146311385+paridhi1509@users.noreply.github.com>
2024-05-18 10:21:25 +0530

    Uploading login

---
+++ loginscript.js
@@ -0,0 +1,57 @@
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const form = document.getElementById("form")
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(username.value);
+    console.log(password.value)
+    let x = 0
+    const relod = function () {
+        location.reload();
+    }
+    for (let i = 0; i < user.length; i++) {
+        if (user[i].user === username.value && user[i].paasword === password.value) {
+            const p = document.createElement('div')
+            p.innerHTML = "Welcome"
+            p.style.textAlign = "center"
+            p.style.textDecoration = "bolder"
+            form.appendChild(p);
+
+            const time = setTimeout(relod, 3000)
+
+            x = 1;
+        }
+        else if (x === 0) {
+            if (i === user.length - 1) {
+                const p = document.createElement('div')
+                p.innerHTML = "Incorrect username or password"
+                p.style.textAlign = "center"
+                p.style.textDecoration = "bolder"
+                form.appendChild(p);
+                const time = setTimeout(relod, 1000)
+
+            }
+        }
+    }
+})
+
+// createing four user data
+
+const user = [{
+    user: "user1",
+    paasword: "abc"
+},
+{
+    user: "user2",
+    paasword: "xyz"
+},
+{
+    user: "user3",
+    paasword: "pqr"
+},
+{
+    user: "user4",
+    paasword: "lmn"
+},
+]
